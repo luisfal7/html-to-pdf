717f64b432f337839c58e37d0ed7bb4bb45a62c1
Luis Falconi <luism.falconi@gmail.com>
2022-10-28 12:01:01 -0300

    prueba commit rama

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h1 className={styles.title}>
+          Prueba rama LF
+        </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
